c61b5a018295239fafe3e404e7a566e02c972056
Redmond Chan <redmondchan92@gmail.com>
2019-01-06 17:11:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -42,7 +42,7 @@ def hit?(num)
 end
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"# code invalid_command here
 end
 
 #####################################################
